dcc024b6f54c26ec8eb5a5bec5a3bc3001500c69
Arvin Singh Kushwaha <38300391+ArvinSKushwaha@users.noreply.github.com>
2020-05-05 18:09:11 -0400

    Oops...

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 import csv
 
-with open("Sem1.csv") as f:
+with open("Sem2.csv") as f:
     courses = list(csv.reader(f))[1:]
     course_names = [i[0] for i in courses]
     names = []
@@ -8,7 +8,7 @@ with open("Sem1.csv") as f:
         course_numbers = i.split(' ')[0]
         course_name = " ".join(i.split(' ')[1:])
         names.append(f"<option value=\"{i}\"></option>")
-        with open(f"Schedule\\Semester1\\{course_numbers}.txt", "w") as f2:
+        with open(f"Schedule\\Semester2\\{course_numbers}.txt", "w") as f2:
             f2.write(course_name + "\n")
             for meetings in set([m[-1] for m in filter(lambda x: x[0]==i, courses)]):
                 f2.write(meetings + "\n")
