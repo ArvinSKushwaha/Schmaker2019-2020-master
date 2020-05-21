ff7048cdc1d516b224ff17ea89290004d157df6a
Arvin Singh Kushwaha <38300391+ArvinSKushwaha@users.noreply.github.com>
2020-05-21 03:11:55 -0400

    update!

--- parse.py
+++ parse.py
@@ -1,15 +1,20 @@
 import csv
+import sys
 
-with open("Sem2.csv") as f:
-    courses = list(csv.reader(f))[1:]
-    course_names = [i[0] for i in courses]
-    names = []
-    for i in sorted(course_names):
-        course_numbers = i.split(' ')[0]
-        course_name = " ".join(i.split(' ')[1:])
-        names.append(f"<option value=\"{i}\"></option>")
-        with open(f"Schedule\\Semester2\\{course_numbers}.txt", "w") as f2:
-            f2.write(course_name + "\n")
-            for meetings in set([m[-1] for m in filter(lambda x: x[0]==i, courses)]):
-                f2.write(meetings + "\n")
-    print("\n".join(sorted(list(set(names)))))
+for i in range(1, 2+1):
+    w = str(i)
+    with open("Sem" + w + ".csv") as f:
+        courses = list(csv.reader(f))[1:]
+        course_names = [i[0] for i in courses]
+        names = []
+        for i in sorted(course_names):
+            course_numbers = i.split(' ')[0]
+            course_name = " ".join(i.split(' ')[1:])
+            names.append(f"<option value=\"{i}\"></option>")
+            with open(f"Schedule\\Semester" + w + f"\\{course_numbers}.txt", "w") as f2:
+                f2.write(course_name + "\n")
+                for meetings in set([m[-1] for m in filter(lambda x: x[0]==i, courses)]):
+                    f2.write(meetings + "\n")
+        
+        with open("SemesterOptions.txt", "a") as f:
+            f.write("".join(sorted(list(set(names))))+"\n")
