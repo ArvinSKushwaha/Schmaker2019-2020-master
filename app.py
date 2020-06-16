653098119cc25b6a784420ba9a509bec8ec3b2f0
Arvin Singh Kushwaha <38300391+ArvinSKushwaha@users.noreply.github.com>
2020-06-16 06:02:15 -0400

    Rewrote

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, render_template, url_for, jsonify
 from multiprocessing import Pool, cpu_count
-# from functools import reduce
+from functools import reduce
+from operator import add
 from itertools import product
 from json import dumps
 from math import floor
 from pprint import pprint
+from collections import defaultdict
+
+KEYS = ["A1", "A2", "A3", "A4", "B1", "B2", "B4", "B5", "C1", "C3", "C4", "C5", "D1", "D2", "D3", "D5", "E1", "E2", "E3", "E5", "F1", "F3", "F4", "F5", "G1", "G3", "G5", "H1", "H2", "H3", "H4", "I1", "I2", "I3", "I4", "AL", "BL", "CL", "DL", "EL", "FL", "GL1", "GL2"]
 
 def gpa_val(x):
     try:
@@ -22,14 +26,18 @@ def gpa_val(x):
         return None
 
 def minutes(sched):
-    if('L' == sched[1]):
-        return 40
-    if(sched[1] in '12345'):
-        return 50
-    else:
-        return 0
+    sched = parse(sched)
+    time = 0
+    for i in sched:
+        if('L' == i[1]):
+            time += 40
+        if(i[1] in '12345' and not i[0] in 'M'):
+            time += 50
+        else:
+            time += 0
+    return time
 
-def parseSchedule(meetings):
+def parse(meetings):
     block = meetings[0]
     if(block not in 'ABCDEFGHI'):
         return []
@@ -47,15 +55,12 @@ def parseSchedule(meetings):
             block = meetings[i+1]
     return blocks
 
-def safe(scheds):
-    sum = []
-    for i in scheds:
-        # print(i)
-        sum += parseSchedule(i) if i else []
-    # print(sum)
-    if(len(set(sum)) == len(sum)):
-        return True
-    return False
+def unique(arr):
+    return len(arr) == len(set(arr))
+
+def safe(combo):
+    m = reduce(add, map(parse, combo))
+    return unique(m)
 
 app = Flask(__name__)
     
@@ -67,184 +72,57 @@ def home2():
     if(request.method == "POST"):
         r = request.form
         rows = len(r)//2
-        classes = [[],[]]
-        cnames = [[],[]]
-        # print(r)
-        for i in range(rows):
-            # print(f'Choice{i+1}')
-            tri1 = r.get(f'sem1Choice{i+1}').split(' ')
-            if(tri1[0]):
-                classes[0].append(tri1[0])
-            tri2 = r.get(f'sem2Choice{i+1}').split(' ')
-            if(tri2[0]):
-                classes[1].append(tri2[0])
-        sem1_meetings = [open(f'Schedule/Semester1/{i}.txt', 'r').read().splitlines()[1:]  for i in classes[0]]
-        sem2_meetings = [open(f'Schedule/Semester2/{i}.txt', 'r').read().splitlines()[1:] for i in classes[1]]
-        # print(tri1_meetings)
-        # print('\n'.join([repr([parseSchedule(x) for x in i]) for i in tri1_meetings]))
-        # print([list(i) for i in product(*tri1_meetings)])
-        # print([safe(x) for x in product(*tri1_meetings)])
-        sems = [map(safe,product(*sem1_meetings)), map(safe,product(*sem2_meetings))]
-        sem1_success = any(sems[0])
-        sem2_success = any(sems[1])
-        print(sem1_meetings, sem2_meetings)
-        numpos = [len(list(i)) for i in sems]
-        numpos = f"Semester 1: {numpos[0]} combinations<br>Semester 2: {numpos[1]} combinations"
-        success = [sem1_success, sem2_success]
-        schedules = [[]]
-        schedule = schedules[0]
-        num1, num2 = 0, 0
-        if(all(success)):
-            for i in product(*sem1_meetings):
-                if(safe(i)):
-                    num1 += 1
-                    schedule.append(i)
-            for i in product(*sem2_meetings):
-                if(safe(i)):
-                    num2 +=1
-                    schedule.append(i)
-            for i in range(len(schedule)):
-                if('VS' in schedule[i]):
-                    schedule[i] = list(schedule[i])
-                    index = schedule[i].index('VS')
-                    schedule[i].pop(index)
-                    classes[i].pop(index)
-            schedule = [[(parseSchedule(pattern), classname) for pattern,classname in zip(*i)] for i in zip(schedule, classes)]
-            print(schedule)
-            if(len(schedule[0]) == 0):
-                num1 = 0
-            if(len(schedule[1]) == 0):
-                num2 = 0
-        schedule2 = [{
-            'A1':'Free Time',
-            'A2':'Free Time',
-            'A3':'Free Time',
-            'A4':'Free Time',
-            'B1':'Free Time',
-            'B2':'Free Time',
-            'B4':'Free Time',
-            'B5':'Free Time',
-            'C1':'Free Time',
-            'C3':'Free Time',
-            'C4':'Free Time',
-            'C5':'Free Time',
-            'D1':'Free Time',
-            'D2':'Free Time',
-            'D3':'Free Time',
-            'D5':'Free Time',
-            'E1':'Free Time',
-            'E2':'Free Time',
-            'E3':'Free Time',
-            'E4':'Free Time',
-            'E5':'Free Time',
-            'F1':'Free Time',
-            'F3':'Free Time',
-            'F4':'Free Time',
-            'F5':'Free Time',
-            'G1':'Free Time',
-            'G3':'Free Time',
-            'G4':'Free Time',
-            'G5':'Free Time',
-            'H1':'Free Time',
-            'H2':'Free Time',
-            'H3':'Free Time',
-            'H4':'Free Time',
-            'I1':'Free Time',
-            'I2':'Free Time',
-            'I3':'Free Time',
-            'I4':'Free Time',
-            'AL':'Free Time',
-            'BL':'Free Time',
-            'CL':'Free Time',
-            'DL':'Free Time',
-            'EL':'Free Time',
-            'FL':'Free Time',
-            'GL1':'Free Time',
-            'GL2':'Free Time',
-        },{
-            'A1':'Free Time',
-            'A2':'Free Time',
-            'A3':'Free Time',
-            'A4':'Free Time',
-            'B1':'Free Time',
-            'B2':'Free Time',
-            'B4':'Free Time',
-            'B5':'Free Time',
-            'C1':'Free Time',
-            'C3':'Free Time',
-            'C4':'Free Time',
-            'C5':'Free Time',
-            'D1':'Free Time',
-            'D2':'Free Time',
-            'D3':'Free Time',
-            'D5':'Free Time',
-            'E1':'Free Time',
-            'E2':'Free Time',
-            'E3':'Free Time',
-            'E4':'Free Time',
-            'E5':'Free Time',
-            'F1':'Free Time',
-            'F3':'Free Time',
-            'F4':'Free Time',
-            'F5':'Free Time',
-            'G1':'Free Time',
-            'G3':'Free Time',
-            'G4':'Free Time',
-            'G5':'Free Time',
-            'H1':'Free Time',
-            'H2':'Free Time',
-            'H3':'Free Time',
-            'H4':'Free Time',
-            'I1':'Free Time',
-            'I2':'Free Time',
-            'I3':'Free Time',
-            'I4':'Free Time',
-            'AL':'Free Time',
-            'BL':'Free Time',
-            'CL':'Free Time',
-            'DL':'Free Time',
-            'EL':'Free Time',
-            'FL':'Free Time',
-            'GL1':'Free Time',
-            'GL2':'Free Time',
-        }
-        ]
-        for i in schedule2:
-            for j in i.keys():
-                i[j] = ''
-        tot = []
-        for i in classes:
-            tot+=i
-        tot = [gpa_val(i) for i in tot]
-        n = 0
-        c = 0
-        for i in tot:
-            if(i):
-                n += i
-                c += 1
-        gpa = n/c
+        pprint(r)
+
+        success = []
+        combo_count = []
+        time_taken = [0, 0]
+        schedules = []
+
+        for semester in range(1, 3):
+            classes = []
+            for choice in range(1, rows+1):
+                option = f"sem{semester}Choice{choice}" # Generates the keys for the request.form dictionary
+                try:
+                    classes.append(r.get(option).split()[0]) # Gives a list of class IDs
+                except IndexError:
+                    classes.append("") # Allow for blank spaces
+            options_per_class = []
+            for c in classes:
+                try:
+                    with open(f"Schedule/Semester{semester}/{c}.txt", "r") as f: # Opens the file containing the meeting patterns
+                        options_per_class.append([i.strip() for i in f.readlines()[1:]]) # Load class options
+                except:
+                    options_per_class.append([" "]) # Allow for blank spaces
+            class_combinations = product(*options_per_class)
+            possible_combos = []
+            for combo in class_combinations:
+                if(safe(combo)):
+                    possible_combos.append(combo)
+            combination_count = len(possible_combos) if all(classes) else 0
+            combo_count.append(combination_count)
+            if(combination_count > 0 or not all(classes)):
+                success.append(True)
+            else:
+                success.append(False)
+            for class_ in possible_combos[0]:
+                time_taken[semester-1] += minutes(class_)
             
-        if(all(success)):
-            for semester in range(2):
-                for i in range(len(schedule[semester])):
-                    ctuple = schedule[semester][i]
-                    for i in ctuple[0]:
-                        schedule2[semester][i] = ctuple[1]
-            # pprint(schedule2)
-        sems = [0,0,0]
-        if(schedule):
-            for i in range(2):
-                for j in schedule[i]:
-                    for k in j[0]:
-                        sems[i] += minutes(k)
-            n = f"Semester 1: {floor(sems[0]/60)} hrs {sems[0]%60} min<br>Semester 2: {floor(sems[1]/60)} hrs {sems[1]%60} min<br>"
-        else:
-            n = "Conflicts Found"
-        print(n)
-        gpa = str(round(gpa,2))
-        gpa = gpa if('.' in gpa) else gpa + '.0'
-        print([num1, num2])
-        return dumps([success, schedule2, gpa, n, numpos, [num1, num2]])
+            schedule = []
+            for curr_opt in possible_combos:
+                curr_sched = dict()
+                for key in KEYS:
+                    curr_sched.update([(key, '')])
+                for c, meet in zip(classes, curr_opt):
+                    for key in parse(meet):
+                        curr_sched[key] = c
+                schedule.append(curr_sched)
+            schedules.append(schedule[-1])
+        pprint(schedules)
+        time_taken = f"Semester 1: {time_taken[0]//60} hrs {time_taken[0]%60} min<br>Semester 2: {time_taken[1]//60} hrs {time_taken[1]%60} min<br>"
+        x = dumps([success,schedules,0,time_taken,0,combo_count])
+        return x
+        
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug = False)
+    app.run(host="0.0.0.0", debug = True)
